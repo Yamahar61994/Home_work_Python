5a1b890d2b0fdee77647c7445d2bf88e470c0e5c
Sergey Nesterov <Nesterovser1994@gmail.ru>
2022-11-25 19:58:45 +0300

    Ispravil

--- Lessons_2/task_1.py
+++ Lessons_2/task_1.py
@@ -3,16 +3,11 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-# num = input('num = ')
-# sum = 0
+num = input('num = ')
+sum = 0
 
-# for i in num:
-#     if i.isdigit(): #проверка, но то, что это именно число, а не символ
-#         sum += int(i)
-# print(f'Summ cifr = {sum}')
+for i in num:
+    if i.isdigit():  # проверка, но то, что это именно число, а не символ
+        sum += int(i)
+print(f'Summ cifr = {sum}')
     
-n = input()
-n = [int(i) for i in n]
-summma = sum(n)
-print(summma)
-print(n)

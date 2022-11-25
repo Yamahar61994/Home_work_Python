77d922106e704d17693202088f64718cb8420c94
Sergey Nesterov <Nesterovser1994@gmail.ru>
2022-11-25 22:05:59 +0300

    Ispravil

--- Lessons_2/task_3.py
+++ Lessons_2/task_3.py
@@ -1,10 +1,14 @@
 # Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
 
+# n = int(input('N = '))
+# s = []
+# for i in range(1, n + 1):
+#     b = round(((1 + 1) / i)**i, 3)
+#     s.append(b)
+# print(s)
+# print(f'Summa = {round(sum(s), 3)}')
+
 n = int(input('N = '))
-s = []
-b = 1
-for i in range(1, n + 1):
-    b = round(((1 + 1) / i)**i, 2)
-    s.append(b)
-print(s)
-print(f'Summa = {round(sum(s), 2)}')
+spi = [(1+1/i)**i for i in range(1, n+1)]
+print(f'Последовательность: {spi}')
+print(f'Сумма: {round(sum(spi), 3)}') 
